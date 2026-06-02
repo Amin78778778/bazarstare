9859818d5e26f79f0ae53d578814ae22a4dbfcd9
Amin78778778 <98718333+Amin78778778@users.noreply.github.com>
2026-06-02 22:57:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
+import asyncio
 import os
 from aiogram import Bot, Dispatcher
-import asyncio
 import db
 
-# Tokeni buraya birbaşa yapışdır (müvəqqəti olaraq)
+# Tokeni birbaşa bura yazırıq (Daha sadə olsun deyə)
 TOKEN = "8853785671:AAF6KXLRBKsif9mnUXUhBJu5-S8gupXBGRQ"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 async def main():
-    # 1. Bazanı avtomatik yaradır/yoxlayır
-    print("Baza yoxlanılır...")
-    await db.init_db()
-    
-    # 2. Botu işə salırıq
     print("Bot işə düşür...")
     await dp.start_polling(bot)
 
